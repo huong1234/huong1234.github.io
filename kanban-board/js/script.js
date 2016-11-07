882ebd7cab751da0b3b0c5e4d67c163a86bc1ed6
huong1234 <sharon.dt@hotmail.com>
2016-11-07 17:19:13 +0700

    1.1
    kanban board

--- kanban-board/js/script.js
+++ kanban-board/js/script.js
@@ -1,4 +1,4 @@
-var app={
+var app={ //tao object app có 2 functions . 
 	newJob :function(e, type, input){
 		console.log("da vao cv moi");
 		var jobName = $(input).val();
@@ -14,26 +14,39 @@ var app={
 	addJobToList: function(type, jobName){
 		console.log("da vao them cv moi");
 		var item='<a href="#!" class="collection-item">' + jobName +
-		'<span class="badge"><i class="tiny material-icons">delete </i></span></a>';
+		'<span class="badge" onclick="app.deleteJob(this)"><i class="tiny material-icons">delete </i></span></a>';
 
 		$('#'+type).append(item);
-		 	} //ctrl + alt + del to format code
-};
+	}, //ctrl + alt + del to format code
+	deleteJob:function(span){
+		var modal= $('#modal1');
+		console.log('vào hàm deleteJob');
+		
+		
+		
+		console.log(modal);
+		modal.openModal();
 
+		var item= $(span).parent();
+
+		$('#btn-delete').on('click', function(){
+		
+			item.remove();
+			modal.closeModal();
+		});
+		
+	}
+};
 $( function() {
 	$( ".sorted-list" ).sortable({
 		connectWith: ".sorted-list",
-    			placeholder:"ui-state-highlight", //tao the div de giu cho khi keo tha
+    	placeholder:"ui-state-highlight", //tao the div de giu cho khi keo tha
     			start:function(event,ui){
     				//add style class
     				$(ui.item[0]).addClass('dragging'); //hai phuong thuc
     			},
     			stop:function(event, ui){
     				$(ui.item[0]).removeClass('dragging');
-
-
-
-    			},
-    			
+    			},    			
     		});
 });
